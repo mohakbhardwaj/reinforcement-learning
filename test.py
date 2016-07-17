98c857b7ad40ad579326cc2d1033243c3e5c1b76
mohak <mohak.bhardwaj@gmail.com>
2016-07-17 19:15:23 -0400

    cross entropy method first comit

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import gym
+env = gym.make('MountainCar-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
